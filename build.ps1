c1ec2dc2d5e7e4d6a9f92617f8cb05741407941f
Martin Buchleitner <mabunixda@gmail.com>
2017-10-17 10:48:57 +0200

    update to new changelog structure

--- build.ps1
+++ build.ps1
@@ -9,23 +9,35 @@
 #{{ChecksumType}} - The checksum type for the url | /ct
 #{{ChecksumTypex64}} - The checksum type for the 64-bit url | /ct64
 
-Write-Progress -Activity "Downloading kubernetes changelog"
+Write-Progress -Activity "Downloading kubernetes changelog"git 
+$baseURI = "https://raw.githubusercontent.com/kubernetes/kubernetes/master"
 
-(Invoke-WebRequest https://raw.githubusercontent.com/kubernetes/kubernetes/master/CHANGELOG.md).Content -split "`n" | Where-Object { 
-    $_ -match "\[kubernetes-node-windows-amd64.tar.gz\]\((?<Url>.*?/v(?<Version>\d+\.\d+\.\d+).*?)\) \| ``(?<Hash>\w+)``" 
+(Invoke-WebRequest $baseURI/CHANGELOG.md).Content -split "`n" | Where-Object { 
+    $_ -match "CHANGELOG-(?<Version>\d+\.\d+)" 
 } | ForEach-Object {
-    $versionInfo = New-Object PSObject -Property @{ "Hash" = $matches["Hash"]; "Url" = $matches["Url"]; "Version" = $matches["Version"] }
-    
-    Write-Progress -Activity "Building package version $($versionInfo.Version)"
-    Get-ChildItem .\kubernetes-node\ -File -Filter *.template -Recurse | ForEach-Object { 
-        $outFile = [System.IO.Path]::Combine($_.DirectoryName, [System.IO.Path]::GetFileNameWithoutExtension($_.FullName))
+    $kubeVersion = $matches["Version"]
 
-        (Select-String -NotMatch "^\s*#" -Path $_.FullName  | Select-Object -ExpandProperty Line) `
-            -replace "{{PackageName}}", "kubernetes-node" `
-            -replace "{{PackageVersion}}", $versionInfo.Version `
-            -replace "{{DownloadUrlx64}}", $versionInfo.Url `
-            -replace "{{Checksumx64}}", $versionInfo.Hash > $outFile
+    Write-Progress -Activity "Downloading kubernetes changelog $kubeVersion"
+    (Invoke-WebRequest $baseURI/CHANGELOG-$kubeVersion.md).Content -split "`n" | Where-Object { 
+        $_ -match "\[kubernetes-node-windows-amd64.tar.gz\]\((?<Url>.*?/v(?<Version>\d+\.\d+\.\d+)(?<VersionFlag>-.*?)\/.*?)\) \| ``(?<Hash>\w+)``" 
+    } | ForEach-Object {
+        $version = $matches["Version"]c
+        if(-not [string]::IsNullOrEmpty($matches["VersionFlag"])) {
+           $version += $matches["VersionFlag"].Replace(".","")
+        }
+          
+        $versionInfo = New-Object PSObject -Property @{ "Hash" = $matches["Hash"]; "Url" = $matches["Url"]; "Version" = $version }
+        Write-Progress -Activity "Building package version $($versionInfo.Version)"
+        Get-ChildItem .\kubernetes-node\ -File -Filter *.template -Recurse | ForEach-Object { 
+            $outFile = [System.IO.Path]::Combine($_.DirectoryName, [System.IO.Path]::GetFileNameWithoutExtension($_.FullName))
+
+            (Select-String -NotMatch "^\s*#" -Path $_.FullName  | Select-Object -ExpandProperty Line) `
+                -replace "{{PackageName}}", "kubernetes-node" `
+                -replace "{{PackageVersion}}", $versionInfo.Version `
+                -replace "{{DownloadUrlx64}}", $versionInfo.Url `
+                -replace "{{Checksumx64}}", $versionInfo.Hash > $outFile
+        }
+
+        choco.exe pack .\kubernetes-node\kubernetes-node.nuspec --version=$($versionInfo.Version)
     }
-
-    choco.exe pack .\kubernetes-node\kubernetes-node.nuspec --version=$($versionInfo.Version)
 }
